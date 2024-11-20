42981445f9bd36e1b38b5f533729dccb50cba33b
J0dany <jodany.fanord@gmail.com>
2024-11-20 01:54:11 +0000

    Info and Profile Changes

---
+++ info_Fanord.py
@@ -0,0 +1,73 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture_1 = "Images/profile_Fanord.jpeg"
+about_me_1 = "I'm Jodany Fanord. I'm a first year Electrical Engineering Major at Georgia Tech interested in Telecommunications and Signal Processing. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url_1 = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url_1 = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url_1 = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url_1 = "https://www.linkedin.com/in/jodany-fanord-6a3037281/"
+my_github_url_1 = "https://github.com/J0dany"
+my_email_address_1 = "jodany.fanord@gmail.com"
+
+
+education_data_1 ={
+    'Degree': 'Bachelor of Science in Electrical Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data_1 = {
+    "code":["CS 1301", "ECE 1101", "MATH 1551", "PHYS 2211", "ENGL 1101"], 
+    "names":["Intro to CS", "ECE Discovery Studio", "Differential Calculus", "Physics 1", "English Composition I"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python is not my favorite snake anymore","Steps towards building my professional persona", "Starting to enter my prime", "Problem Solving and Critical Thinking", "Developing beautiful prose one essay at a time"],
+    }
+experience_data_1 = {
+    "Data Analyst at Non Profit Organization": (["- Analyzed and interpreted social media trends and their impact on organizational engagement, applying data-driven insights to enhance online presence and strategic outreach",
+                                                                          "- Conducted detailed data analysis of donation and attendance metrics", "Developed sophisticated data visualizations to track and present organizational growth and trends"],"Images/cook.jpg"),
+    "CS Teacher's Assistant at Oliver Scholars":(["- Taught algebra and Python to 15-20 students, focusing on problem-solving and computational thinking.",
+                                                           "- Provided tutoring and feedback to strengthen students' math and coding skills for engineering applications."],"Images/cleaner.jpg"),
+}
+
+projects_data_1 = {
+    "Arduino Robotic Arm": "Designed and Programmed 3-d printed  Arduino-Powered Robotic Arm with object tracking integration",
+}
+
+programming_data_1 = {
+    "Python": 90,
+    "Java": 70,
+    "C++": 30,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons_1 = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C++": "🔍",
+}
+spoken_icons_1 = {
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data_1 = {
+    "English": "Fluent",
+    "Spanish": "Intermediate",
+}
+leadership_data_1 = {
+    "GTSBE LDR Fundraising Committee": (["- Collaborate with club members to plan events and fundraisers that will raise money for the on-campus club for Black Engineering Freshmen."],"Images/puff.jpg"),
+
+}
+activity_data_1={
+    "Student AV/IT": ["Assist on campus with AV and IT tech based issues, working along side student and full time employees", 
+            "-I specialize in maintaining equipment, troubleshooting common issues, and acquiring skills in the IT field."]
+}
--- pages/2_Jodany_Fanord_Portfolio.py
+++ pages/2_Jodany_Fanord_Portfolio.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 def about_me_section():
     st.header("About Me")
-    st.image(info.profile_picture, width = 200)
-    st.write(info.about_me)
+    st.image(info.profile_picture_1, width = 200)
+    st.write(info.about_me_1)
     st.write("---")
 about_me_section()
 
@@ -15,24 +15,24 @@ about_me_section()
 def links_section():
     st.sidebar.header("Links")
     st.sidebar.text("Connect with me on Linkedin")
-    linkedin_link = f'<a href="{info.my_linkedin_url}"><img src="{info.linkedin_image_url}" alt="LinkedIn" width = "75" height="75"></a>'
+    linkedin_link = f'<a href="{info.my_linkedin_url_1}"><img src="{info.linkedin_image_url}" alt="LinkedIn" width = "75" height="75"></a>'
     st.sidebar.markdown(linkedin_link, unsafe_allow_html=True)
     st.sidebar.text("Check Out My Work")
-    github_link = f'<a href="{info.my_github_url}"><img src="{info.github_image_url}" alt="Github" width = "65" height="65"></a>'
+    github_link = f'<a href="{info.my_github_url_1}"><img src="{info.github_image_url}" alt="Github" width = "65" height="65"></a>'
     st.sidebar.markdown(github_link, unsafe_allow_html=True)
     st.sidebar.text("Email Me!")
-    email_html = f'<a href="mailto:{info.my_email_address}"><img src="{info.email_image_url}" alt="Email" width = "75" height="75"></a>'
+    email_html = f'<a href="mailto:{info.my_email_address_1}"><img src="{info.email_image_url}" alt="Email" width = "75" height="75"></a>'
     st.sidebar.markdown(email_html, unsafe_allow_html=True)
 links_section()
 #Education
 def education_section(education_data,course_data):
     st.header("Education")
-    st.subheader(f"**{education_data['Institution']}**")
-    st.write(f"**Degree:**{education_data['Degree']}")
-    st.write(f"**Graduation Date:**{education_data['Graduation Date']}")
-    st.write(f"**GPA:**{education_data['GPA']}")
+    st.subheader(f"**{education_data_1['Institution']}**")
+    st.write(f"**Degree:**{education_data_1['Degree']}")
+    st.write(f"**Graduation Date:**{education_data_1['Graduation Date']}")
+    st.write(f"**GPA:**{education_data_1['GPA']}")
     st.write("**Relevant Coursework:**")
-    coursework = pd.DataFrame(course_data)
+    coursework = pd.DataFrame(course_data_1)
     st.dataframe(coursework, column_config={
         "code": "Course Code",
         "names" : "Course Names",
@@ -53,7 +53,7 @@ def experience_section(experience_data):
         for bullet in job_description:
             expander.write(bullet)
     st.write("---")
-experience_section(info.experience_data)
+experience_section(info.experience_data_1)
 
 #Projects
 
@@ -63,7 +63,7 @@ def project_section(projects_data):
         expander=st.expander(f"{project_name}")
         expander.write(project_description)
     st.write("---")
-project_section(info.projects_data)
+project_section(info.projects_data_1)
 
 
 
@@ -87,21 +87,21 @@ def activities_section(leadership_data, activity_data):
             for bullet in details:
                 expander.write(bullet)
     st.write("---")
-activities_section(info.leadership_data, info.activity_data)
+activities_section(info.leadership_data_1, info.activity_data_1)
 
 #Skills
 def skills_section(programming_data, spoken_data):
     st.header("Skills")
     st.subheader("Programming Languages")
     for skill,percentage in programming_data.items():
-        st.write(f"{skill}{info.programming_icons.get(skill,'')}")
+        st.write(f"{skill}{info.programming_icons_1.get(skill,'')}")
         st.progress(percentage)
     st.subheader("Spoken Languages")
     for spoken,proficiency in spoken_data.items():
-        st.write(f"{spoken}{info.spoken_icons.get(spoken, '')}:{proficiency}")
+        st.write(f"{spoken}{info.spoken_icons_1.get(spoken, '')}:{proficiency}")
     st.write("---")
 
-skills_section(info.programming_data, info.spoken_data)
+skills_section(info.programming_data_1, info.spoken_data_1)
 
 
 
