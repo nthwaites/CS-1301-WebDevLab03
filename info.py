fc9bf8191d8e3d123826b82f82189f647bbd441a
nthwaites <n.thwaites456@gmail.com>
2024-11-20 03:48:34 +0000

    info redo

---
+++ info.py
@@ -0,0 +1,146 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm SpongeBobe Square Pants. I work at the Krusty Krab and I make fantastic Krabby Patties. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
+my_github_url = "https://github.com/spongebob-squarepants"
+my_email_address = "spongebob@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["Python is not my favorite snake anymore", "OOP(S!) (Java Edition)", "Basically row reduction", "If only my grade was discrete too..."],
+    }
+experience_data = {
+    "Cook at Krusty Krab": (["- Worked on designing the Krabby Patty Recipe",
+                                                                          "- Increased Company Revenue by 200 dollars", "- Made Mr. Krabs Proud"],"Images/cook.jpg"),
+    "Cleaner at Krusty Krab":(["- Made the floor almost reflective",
+                                                           "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
+    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "Krabby Patty Recipe": "Created the famous Recipe and tailored it from scratch",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Fluent",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "Driver at Mrs Puff's School": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
+
+}
+activity_data={
+    "SpongeBob and Patrick Friendship Club": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+}
+
+profile_picture_1 = "Images/profile_Fanord.jpeg"
+about_me_1 = "I'm Jodany Fanord. I'm a first year Electrical Engineering Major at Georgia Tech interested in Telecommunications and Signal Processing. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url_1 = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url_1 = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url_1 = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url_1 = "https://www.linkedin.com/in/jodany-fanord-6a3037281/"
+my_github_url_1 = "https://github.com/J0dany"
+my_email_address_1 = "jodany.fanord@gmail.com"
+
+
+education_data_1 ={
+    'Degree': 'Bachelor of Science in Electrical Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data_1 = {
+    "code":["CS 1301", "ECE 1101", "MATH 1551", "PHYS 2211", "ENGL 1101"], 
+    "names":["Intro to CS", "ECE Discovery Studio", "Differential Calculus", "Physics 1", "English Composition I"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python is not my favorite snake anymore","Steps towards building my professional persona", "Starting to enter my prime", "Problem Solving and Critical Thinking", "Developing beautiful prose one essay at a time"],
+    }
+experience_data_1 = {
+    "Data Analyst at Non Profit Organization": (["- Analyzed and interpreted social media trends and their impact on organizational engagement, applying data-driven insights to enhance online presence and strategic outreach",
+                                                                          "- Conducted detailed data analysis of donation and attendance metrics", "Developed sophisticated data visualizations to track and present organizational growth and trends"],"Images/cook.jpg"),
+    "CS Teacher's Assistant at Oliver Scholars":(["- Taught algebra and Python to 15-20 students, focusing on problem-solving and computational thinking.",
+                                                           "- Provided tutoring and feedback to strengthen students' math and coding skills for engineering applications."],"Images/cleaner.jpg"),
+}
+
+projects_data_1 = {
+    "Arduino Robotic Arm": "Designed and Programmed 3-d printed  Arduino-Powered Robotic Arm with object tracking integration",
+}
+
+programming_data_1 = {
+    "Python": 90,
+    "Java": 70,
+    "C++": 30,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons_1 = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C++": "🔍",
+}
+spoken_icons_1 = {
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data_1 = {
+    "English": "Fluent",
+    "Spanish": "Intermediate",
+}
+leadership_data_1 = {
+    "GTSBE LDR Fundraising Committee": (["- Collaborate with club members to plan events and fundraisers that will raise money for the on-campus club for Black Engineering Freshmen."],"Images/puff.jpg"),
+
+}
+activity_data_1={
+    "Student AV/IT": ["Assist on campus with AV and IT tech based issues, working along side student and full time employees", 
+            "-I specialize in maintaining equipment, troubleshooting common issues, and acquiring skills in the IT field."]
+}
